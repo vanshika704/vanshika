f19b14ec55f0cfb136f6db5aa7ce8e6420eb5007
vanshika704 <sharmavanshi704@gmail.com>
2024-07-31 10:55:22 +0530

    .

--- src/components/vantanet.jsx
+++ src/components/vantanet.jsx
@@ -52,12 +52,14 @@ class MyComponent1 extends React.Component {
       <div ref={this.vantaRef} style={style}>
         <div><Navbar/></div>
 
-        <div className="image-container">
+       <div className='center'>
           <div className='poppins-bold text'>Hi Everyone ! <br></br>
           I am Vanshika <br></br>
-          Front End Developer</div>
+          Front End Developer </div>
+          <div className="image-container">
           <img src="src/assets/IMG-20240117-WA0060.jpg" alt="example" />
         </div>
+      </div>
       </div>
     );
   }
