ff82e315e43637aeb2ba68200a4eb98b9d8f2bd9
vanshika704 <sharmavanshi704@gmail.com>
2024-08-07 22:56:01 +0530

    .

--- src/components/ui/aboutme.jsx
+++ src/components/ui/aboutme.jsx
@@ -1,26 +1,28 @@
-// src/pages/AboutMe.js
-
+// src/components/ui/aboutme.js
 import HTMLFlipBook from "react-pageflip";
+ // Make sure to import the CSS file for styling
 
 function MyBook() {
-  return (<div className="bookContainer">
-    <HTMLFlipBook
-      width={300}
-      height={533}
-      size="stretch"
-      minWidth={315}
-      maxWidth={300}
-      minHeight={420}
-      maxHeight={950}
-      maxShadowOpacity={0.5}
-      showCover={true}
-      mobileScrollSupport={false}
-    >
-      <div className="demoPage">Hi, my name is Vanshika.</div>
-      <div className="demoPage">I am a web developer.</div>
-      <div className="demoPage">I love working with React.js.</div>
-      <div className="demoPage">I enjoy creating interactive UIs.</div>
-    </HTMLFlipBook></div>
+  return (
+    <div className="bookContainer">
+      <HTMLFlipBook
+        width={300} // Increased width
+        height={500} // Increased height
+        size="stretch"
+        minWidth={300}
+        maxWidth={300}
+        minHeight={500}
+        maxHeight={500}
+        maxShadowOpacity={0.5}
+        showCover={true}
+        mobileScrollSupport={false}
+      >
+        <div className="demoPage">Hi, my name is Vanshika.</div>
+        <div className="demoPage">I am a web developer.</div>
+        <div className="demoPage">I love working with React.js.</div>
+        <div className="demoPage">I enjoy creating interactive UIs.</div>
+      </HTMLFlipBook>
+    </div>
   );
 }
 
