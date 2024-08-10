096167bb96db7555eec947bbd983dead202623f1
vanshika704 <sharmavanshi704@gmail.com>
2024-08-10 12:27:03 +0530

    grid1

--- src/components/ui/aboutme.jsx
+++ src/components/ui/aboutme.jsx
@@ -15,7 +15,7 @@ function MyBook() {
         <GridItem 
           rowSpan={{ base: 1, sm: 2, md: 2 }} 
           colSpan={{ base: 2, sm: 3, md: 1 }} 
-          bg='papayawhip' 
+         bg='rgba(255, 239, 213, 0.3)'
           display="flex" 
           alignItems="center" 
           justifyContent="center"
@@ -30,16 +30,15 @@ function MyBook() {
               showCover={true}
               mobileScrollSupport={false}
             >
-              <div className="demoPage demopage3">
-                <div>Vanshika <br />Projects</div>
-              </div>
-              <div className="demoPage demopage1"></div>
-              <div className="demoPage demopage2">I love working with React.js.</div>
-              <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
+            <div className="demoPage demopage1"> <img className="gridimage1" src="src/assets/whatsapp/Screenshot_2024-08-08-20-05-29-42_dd8350f2a4d58b9e26f0ed7fbe7df0d9.jpg" height="400px"></img></div>
+            <div className="demoPage demopage1"> <img className="gridimage1" src="src/assets/whatsapp/Screenshot_2024-08-08-20-05-44-46_dd8350f2a4d58b9e26f0ed7fbe7df0d9.jpg" height="400px"></img></div>
+            <div className="demoPage demopage1"> <img className="gridimage1" src="src/assets/whatsapp/Screenshot_2024-08-08-20-05-52-27_dd8350f2a4d58b9e26f0ed7fbe7df0d9.jpg" height="400px"></img></div>
+            <div className="demoPage demopage1"> <img className="gridimage1" src="src/assets/whatsapp/Screenshot_2024-08-08-20-05-59-14_dd8350f2a4d58b9e26f0ed7fbe7df0d9.jpg" height="400px"></img></div>
+
             </HTMLFlipBook>
           </div>
         </GridItem>
-        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip'> <div style={{ width: '100%', height: '100%' }}>
+        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)'> <div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -49,25 +48,25 @@ function MyBook() {
               mobileScrollSupport={false}
             >
               <div className="demoPage demopage3">
-              <img src="src/assets/foresthill/Screenshot (73).png" height="280px"></img>
+              <img className="gridimage" src="src/assets/foresthill/Screenshot (73).png" height="280px"></img>
               </div>
-              <div className="demoPage demopage1"> <img src="src/assets/foresthill/Screenshot (74).png" height="280px"></img></div>
-              <div className="demoPage demopage2"> <img src="src/assets/foresthill/Screenshot (75).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (76).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (77).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (78).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (79).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (80).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (81).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (82).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (83).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (84).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (85).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (86).png" height="280px"></img></div>
-              <div className="demoPage demopage4"> <img src="src/assets/foresthill/Screenshot (87).png" height="280px"></img></div>
+              <div className="demoPage demopage1"> <img className="gridimage" src="src/assets/foresthill/Screenshot (74).png" height="400px"></img></div>
+              <div className="demoPage demopage2"> <img className="gridimage" src="src/assets/foresthill/Screenshot (75).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (76).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (77).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (78).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (79).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (80).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (81).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (82).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (83).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (84).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (85).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage" src="src/assets/foresthill/Screenshot (86).png" height="280px"></img></div>
+              <div className="demoPage demopage4"> <img className="gridimage"  src="src/assets/foresthill/Screenshot (87).png" height="280px"></img></div>
             </HTMLFlipBook>
           </div></GridItem>
-          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip'> <div style={{ width: '100%', height: '100%' }}>
+          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)'> <div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -76,15 +75,19 @@ function MyBook() {
               showCover={true}
               mobileScrollSupport={false}
             >
-              <div className="demoPage demopage3">
-                <div>Vanshika <br />Projects</div>
+             <div className="demoPage ">
+             <img className="gridimage" src = "src/assets/pratishtha/Screenshot (53).png"></img>
               </div>
-              <div className="demoPage demopage1">I am a web developer.</div>
-              <div className="demoPage demopage2">I love working with React.js.</div>
-              <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
+             
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (53).png" className="gridimage"></img></div>
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (54).png" className="gridimage"></img></div>
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (55).png" className="gridimage"></img></div>
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (56).png" className="gridimage"></img></div>
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (57).png" className="gridimage"></img></div>
+              <div className="demoPage demopage1">   <img src = "src/assets/pratishtha/Screenshot (58).png" className="gridimage"></img></div>
             </HTMLFlipBook>
           </div></GridItem>
-          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip'> <div style={{ width: '100%', height: '100%' }}>
+          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)'> <div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -93,15 +96,13 @@ function MyBook() {
               showCover={true}
               mobileScrollSupport={false}
             >
-              <div className="demoPage demopage3">
-                <div>Vanshika <br />Projects</div>
-              </div>
-              <div className="demoPage demopage1">I am a web developer.</div>
-              <div className="demoPage demopage2">I love working with React.js.</div>
-              <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
+           <div className="demoPage demopage1"> <img className="gridimage" src="src/assets/gdscweather/Screenshot (88).png" height="400px"></img></div>
+             <div className="demoPage demopage1"> <img className="gridimage" src="src/assets/gdscweather/Screenshot (89).png" height="400px"></img></div>
+             <div className="demoPage demopage1"> <img className="gridimage" src="src/assets/gdscweather/Screenshot (90).png" height="400px"></img></div>
+             <div className="demoPage demopage1"> <img className="gridimage" src="src/assets/gdscweather/Screenshot (91).png" height="400px"></img></div>
             </HTMLFlipBook>
           </div></GridItem>
-          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip'> <div style={{ width: '100%', height: '100%' }}>
+          <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)'> <div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -131,7 +132,7 @@ function MyBook() {
         <GridItem 
           rowSpan={{ base: 1, sm: 2, md: 2 }} 
           colSpan={{ base: 2, sm: 3, md: 1 }} 
-          bg='papayawhip' 
+          bg='rgba(255, 239, 213, 0.3)' 
           display="flex" 
           alignItems="center" 
           justifyContent="center"
@@ -153,7 +154,7 @@ function MyBook() {
           <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
         </HTMLFlipBook>
       </div></GridItem>
-        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip' ><div style={{ width: '100%', height: '100%' }}>
+        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)' ><div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -170,7 +171,7 @@ function MyBook() {
               <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
             </HTMLFlipBook>
           </div></GridItem>
-        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip' ><div style={{ width: '100%', height: '100%' }}>
+        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)' ><div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -187,7 +188,7 @@ function MyBook() {
               <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
             </HTMLFlipBook>
           </div></GridItem>
-        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip' ><div style={{ width: '100%', height: '100%' }}>
+        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }}  bg='rgba(255, 239, 213, 0.3)' ><div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
@@ -204,7 +205,7 @@ function MyBook() {
               <div className="demoPage demopage4">I enjoy creating interactive UIs.</div>
             </HTMLFlipBook>
           </div></GridItem>
-        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='papayawhip' ><div style={{ width: '100%', height: '100%' }}>
+        <GridItem colSpan={{ base: 2, sm: 3, md: 2 }} bg='rgba(255, 239, 213, 0.3)' ><div style={{ width: '100%', height: '100%' }}>
             <HTMLFlipBook
               width={300} // Define fixed width
               height={280} // Define fixed height
