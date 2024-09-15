60c1e9643e12f39d0cdeb984dad6e3418ade6fda
vanshika704 <sharmavanshi704@gmail.com>
2024-09-15 17:26:37 +0530

    gfoetm

--- src/components/vantanet.jsx
+++ src/components/vantanet.jsx
@@ -1,7 +1,6 @@
-import  { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import NET from 'vanta/dist/vanta.net.min'; // Make sure the Vanta effect you want is imported
-import "../index.css";
 import Navbar from './navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -38,35 +37,26 @@ const MyComponent1 = () => {
     navigate('/about');
   };
 
-  const style = {
-    width: '100vw',
-    height: '100vh',
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    overflow: 'hidden',
-  };
-
   return (
-    <div ref={vantaRef} style={style}>
+    <div ref={vantaRef} className="w-screen h-screen absolute top-0 left-0 overflow-hidden">
       <div><Navbar /></div>
 
-      <div className='center'>
-        <div className='poppins-bold text'>
+      <div className="flex flex-row items-start mt-20 ml-12 w-full max-w-screen-lg">
+        <div className="text-5xl font-bold mb-12 ml-12 mt-5">
           Hi Everyone !!! <br />
           I am Vanshika <br />
-          FrontEnd<br/>
-          <div className='developer'>
-             Developer 
-            <img src='src/assets/pixel-art-12601_256.gif' className='icon' />
+          FrontEnd<br />
+          <div className="flex items-center">
+            Developer
+            <img src='src/assets/pixel-art-12601_256.gif' className='ml-2 mb-2 h-24' alt="icon" />
           </div>
-          <div className='button-container'>
+          <div className="flex space-x-4 mt-4">
             <button type="button" className="btn btn-dark resume">Resume</button>
             <button type="button" className="btn btn-dark resume" onClick={handleExploreClick}>Explore</button>
           </div>
         </div>
-        <div className="image-container ">
-          <img src="src/assets/IMG-20240117-WA0060.jpg" alt="example" />
+        <div className="flex absolute right-1 h-full mb-52">
+          <img src="src/assets/IMG-20240117-WA0060.jpg" alt="example" className="rounded-full h-96 w-96  transition-transform duration-300 ease-in-out shadow-lg hover:translate-y-5 hover:shadow-2xl" />
         </div>
       </div>
     </div>
