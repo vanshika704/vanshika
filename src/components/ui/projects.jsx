4aaa9e664eb49bf5695d2a54b0bd5cd741c0b350
vanshika704 <sharmavanshi704@gmail.com>
2024-09-17 20:34:22 +0530

    f

--- src/components/ui/projects.jsx
+++ src/components/ui/projects.jsx
@@ -3,54 +3,55 @@
 import { PinContainer } from "./3-dpin";
 
 export function AnimatedPinDemo() {
-  return (<><div className="flex"><div className="h-[40rem] w-full flex items-center justify-center">
-    <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
-      <div className="relative flex  p-1 w-[15rem] h-[15rem] overflow-hidden">
-        <img 
-          src="src/assets/foresthill/Screenshot (73).png" 
-          alt="Forest Hill" 
-          className="absolute inset-0 w-full h-full object-cover" 
-        />
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      <div className="flex items-center justify-center h-[40rem] w-full">
+        <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
+          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
+            <img
+              src="src/assets/foresthill/Screenshot (73).png"
+              alt="Forest Hill"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+        </PinContainer>
       </div>
-    </PinContainer>
-  </div>
-  <div className="h-[40rem] w-full flex items-center justify-center">
-    <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
-      <div className="relative flex  p-1 w-[15rem] h-[15rem] overflow-hidden">
-        <img 
-          src="src/assets/foresthill/Screenshot (73).png" 
-          alt="Forest Hill" 
-          className="absolute inset-0 w-full h-full object-cover" 
-        />
+
+      <div className="flex items-center justify-center h-[40rem] w-full">
+        <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
+          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
+            <img
+              src="src/assets/foresthill/Screenshot (73).png"
+              alt="Forest Hill"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+        </PinContainer>
       </div>
-    </PinContainer>
-  </div>
-  <div className="h-[40rem] w-full flex items-center justify-center">
-    <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
-      <div className="relative flex  p-1 w-[15rem] h-[15rem] overflow-hidden">
-        <img 
-          src="src/assets/foresthill/Screenshot (73).png" 
-          alt="Forest Hill" 
-          className="absolute inset-0 w-full h-full object-cover" 
-        />
+
+      <div className="flex items-center justify-center h-[40rem] w-full">
+        <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
+          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
+            <img
+              src="src/assets/foresthill/Screenshot (73).png"
+              alt="Forest Hill"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+        </PinContainer>
       </div>
-    </PinContainer>
-  </div>
- 
-  </div>
- <div><div className="h-[40rem] w-full flex items-center justify-center">
-   <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
-     <div className="relative flex  p-1 w-[15rem] h-[15rem] overflow-hidden">
-       <img 
-         src="src/assets/foresthill/Screenshot (73).png" 
-         alt="Forest Hill" 
-         className="absolute inset-0 w-full h-full object-cover" 
-       />
-     </div>
-   </PinContainer>
- </div></div>  
-  </>
-    
-    
+
+      <div className="flex items-center justify-center h-[40rem] w-full">
+        <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
+          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
+            <img
+              src="src/assets/foresthill/Screenshot (73).png"
+              alt="Forest Hill"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+          </div>
+        </PinContainer>
+      </div>
+    </div>
   );
 }
