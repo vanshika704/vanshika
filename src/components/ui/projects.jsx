ff6686ef7c7232fe23d229e0b3a078a547bf2b2a
vanshika704 <sharmavanshi704@gmail.com>
2024-09-17 21:04:24 +0530

    ethrg

--- src/components/ui/projects.jsx
+++ src/components/ui/projects.jsx
@@ -1,69 +1,101 @@
 "use client";
 
 import { PinContainer } from "./3-dpin";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules"; // Import Autoplay
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 export function AnimatedPinDemo() {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 p-1">
-      <div className="flex items-center justify-center h-[40rem] w-full">
-        <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
-          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
-            <img
-              src="src/assets/foresthill/Screenshot (73).png"
-              alt="Forest Hill"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
-          </div>
-        </PinContainer>
-      </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={70}
+      pagination={{ clickable: true }}
+      navigation={true}
+      autoplay={{ 
+        delay: 1000, // Delay between slides in milliseconds
+        disableOnInteraction: false, // Allow autoplay to continue after user interaction
+      }}
+      speed={1000} // Speed of transition between slides
+      breakpoints={{
+        640: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 4 },
+      }}
+      modules={[Navigation, Pagination, Autoplay]} // Include Autoplay module
+      className="mySwiper"
+    >
+      <SwiperSlide>
+        <div className="flex items-center justify-center h-[30rem] w-full"> {/* Adjusted height */}
+          <PinContainer title="ForestHill" href="https://twitter.com/mannupaaji">
+            <div className="relative flex p-2 w-[20rem] h-[20rem] overflow-hidden"> {/* Adjusted size */}
+              <img
+                src="src/assets/foresthill/Screenshot (73).png"
+                alt="Forest Hill"
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            </div>
+          </PinContainer>
+        </div>
+      </SwiperSlide>
 
-      <div className="flex items-center justify-center h-[40rem] w-full">
-        <PinContainer title="Sign up" href="https://twitter.com/mannupaaji">
-          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
-            <img
-              src="src/assets/all/Screenshot (64).png"
-              alt="Sign up"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
-          </div>
-        </PinContainer>
-      </div>
+      <SwiperSlide>
+        <div className="flex items-center justify-center h-[30rem] w-full"> {/* Adjusted height */}
+          <PinContainer title="Sign up" href="https://twitter.com/mannupaaji">
+            <div className="relative flex p-2 w-[20rem] h-[20rem] overflow-hidden"> {/* Adjusted size */}
+              <img
+                src="src/assets/all/Screenshot (64).png"
+                alt="Sign up"
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            </div>
+          </PinContainer>
+        </div>
+      </SwiperSlide>
 
-      <div className="flex items-center justify-center h-[40rem] w-full">
-        <PinContainer title="Exponentially" href="https://twitter.com/mannupaaji">
-          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
-            <img
-              src="src/assets/exponentially/Screenshot (63).png"
-              alt="Exponentially"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
-          </div>
-        </PinContainer>
-      </div>
+      <SwiperSlide>
+        <div className="flex items-center justify-center h-[30rem] w-full"> {/* Adjusted height */}
+          <PinContainer title="Exponentially" href="https://twitter.com/mannupaaji">
+            <div className="relative flex p-2 w-[20rem] h-[20rem] overflow-hidden"> {/* Adjusted size */}
+              <img
+                src="src/assets/exponentially/Screenshot (63).png"
+                alt="Exponentially"
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            </div>
+          </PinContainer>
+        </div>
+      </SwiperSlide>
 
-      <div className="flex items-center justify-center h-[40rem] w-full">
-        <PinContainer title="Weather app" href="https://twitter.com/mannupaaji">
-          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
-            <img
-              src="src/assets/exponentially/Screenshot (59).png"
-              alt="Weather app"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
-          </div>
-        </PinContainer>
-      </div>
+      <SwiperSlide>
+        <div className="flex items-center justify-center h-[30rem] w-full"> {/* Adjusted height */}
+          <PinContainer title="Weather app" href="https://twitter.com/mannupaaji">
+            <div className="relative flex p-2 w-[20rem] h-[20rem] overflow-hidden"> {/* Adjusted size */}
+              <img
+                src="src/assets/exponentially/Screenshot (59).png"
+                alt="Weather app"
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            </div>
+          </PinContainer>
+        </div>
+      </SwiperSlide>
 
-      <div className="flex items-center justify-center h-[40rem] w-full">
-        <PinContainer title="Pratishtha" href="https://twitter.com/mannupaaji">
-          <div className="relative flex p-1 w-[15rem] h-[15rem] overflow-hidden">
-            <img
-              src="src/assets/pratishtha/Screenshot (46).png"
-              alt="Pratishtha"
-              className="absolute inset-0 w-full h-full object-cover"
-            />
-          </div>
-        </PinContainer>
-      </div>
-    </div>
+      <SwiperSlide>
+        <div className="flex items-center justify-center h-[30rem] w-full"> {/* Adjusted height */}
+          <PinContainer title="Pratishtha" href="https://twitter.com/mannupaaji">
+            <div className="relative flex p-2 w-[20rem] h-[20rem] overflow-hidden"> {/* Adjusted size */}
+              <img
+                src="src/assets/pratishtha/Screenshot (46).png"
+                alt="Pratishtha"
+                className="absolute inset-0 w-full h-full object-cover"
+              />
+            </div>
+          </PinContainer>
+        </div>
+      </SwiperSlide>
+    </Swiper>
   );
 }
